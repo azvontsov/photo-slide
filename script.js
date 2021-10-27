2bd314f35b0cf4a1bbdf1427c5c83227bd08cf2f
Anton <anton@Polinas-MacBook-Air.local>
2021-10-27 18:27:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const BASE_URL;
+const ACCESS_KEY = 'rkrSv1XuU2X9HkbWy4MPSie0POHYly8rboK8TakUdek';
+const SECRET_KEY = 'SZaughnHucUAesEmfVINR1GvvgokO3iI2WpHGbdeHfw';
+
+
+// State Variables
+
+// Cached Element References
+
+// Event Listeners
+
+// Functions
+
