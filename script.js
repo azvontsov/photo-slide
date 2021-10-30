e9b2f200ba0f1754ec5b82df69e765fd022d68a8
Anton <anton@Polinas-MacBook-Air.local>
2021-10-29 23:31:10 -0400

    updated css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://api.unsplash.com/search/photos?per_page=12';
+const BASE_URL = 'https://api.unsplash.com/search/photos?per_page=22';
 const ACCESS_KEY = 'VoHccXXYCed_4Z4O3I8pqg5EY3BRyhnDgQNyebO06uU';
 const SECRET_KEY = 'JTYfOsrpI6MBDV42p-TcSXtt83_yFROgpi0kezvRel0';
 
@@ -15,9 +15,13 @@ const $form = $('form');
 const $input = $('input');
 const $main = $('main');
 
+const $article = $('article');
+
 // Event Listeners
 $form.on('submit', handleSubmit);
 $input.on('click', 'submit', handleSubmit);
+
+$article.on('dblclick', handleSubmit);
 
 
 // Functions
@@ -31,6 +35,8 @@ function handleSubmit(evt) {
             photoData = data;
             render();
             // console.log('data', data);
+$article()
+
         }, function (error) {
 
         });
@@ -41,10 +47,15 @@ function handleSubmit(evt) {
                 const resultHtml = photoData.results.map((result) => {
                   return `
                   <article style="background-image: url(${result.urls.raw})">
-                <h4>${result.alt_description}</h4>
+                  
+                <h6>${result.alt_description}</h6>
+               
                 <h4>❤ ${result.likes}</h4>
                  </article>`
                 }).join('');
-                  
+
+                
+
                  $main.html(resultHtml);
                 }
+                
